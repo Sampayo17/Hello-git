31a9165b995ad9ee6751b04010ec39496f2538fc
sampayo123 <israel_sampayo@hotmail.com>
2023-12-28 12:48:39 -0400

    Prueba desde user 2

---
+++ PracticaGit.js
@@ -0,0 +1,4 @@
+function hellogit(){
+    return console.log("hola desde practica git");
+
+}
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 function hellogit(){
     return console.log("New hello git para subir a github");
+    return console.log("Probando cambios de codigo desde un git clone");
+    return console.log("New hello git para subir a github");
 }
